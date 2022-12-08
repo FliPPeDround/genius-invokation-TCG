8b876b86ce86ae62db9a92a1f8969e9baf9645b1
flippedround <734243792@qq.com>
2022-12-08 22:53:20 +0800

    first commit

---
+++ src/components/card/index.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+interface Props {
+  character: string
+  hp: number
+}
+const { character, hp } = defineProps<Props>()
+
+const getCharacterSrc = (name: string): string => {
+  const picModules = import.meta.glob('./character/*.png', { eager: true })
+  const path = `./character/${name}.png`
+  return (picModules[path]! as { default: string }).default
+}
+</script>
+
+<template>
+  <div
+    relative
+    w-20vw h-34vw
+    m="t-2vw l-1.2vw"
+  >
+    <img
+      :src="getCharacterSrc(character)"
+      absolute
+      w-20vw h-34vw
+    >
+    <div
+      class="bg-bar"
+    />
+    <div
+      class="hp-icon"
+    >
+      {{ hp }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-bar{
+  width: 20vw;
+  height: 34.1vw;
+  background: url(/cards/card_bg.png);
+  background-size: 100% 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.hp-icon {
+  position: absolute;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  font-size: 4.5vw;
+  color: #fff;
+  background-size: 100% 100%;
+  width: 6.4vw;
+  height: 7.4vw;
+  left: -1vw;
+  top: -1.9vw;
+  background-image:url(/cards/hp_icon.png);
+  font-weight: 500;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div flex>
+
+    <card character="Keqing" :hp="10"/>
+    <card character="Fischl" :hp="10" />
+  </div>
+</template>
