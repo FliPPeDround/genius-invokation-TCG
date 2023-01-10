e05ede6d327cdee2d3e7a787d0e168d54c91733e
flippedround <734243792@qq.com>
2023-01-10 16:26:43 +0800

    feat: 优化登录页面样式

--- Client/src/pages/user/login.vue
+++ Client/src/pages/user/login.vue
@@ -1,95 +1,107 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const showPassword = ref(false)
+const isErrorEmail = ref(false)
+const emailValue = ref('')
+const password = ref('')
+
+const emitChangeFn = async () => {
+  if (isEmail(emailValue.value)) {
+    const isRegister = await getIsRegisteredByEmail(emailValue.value)
+    if (isRegister.data) {
+      showPassword.value = true
+      isErrorEmail.value = false
+    }
+  }
+  else {
+    isErrorEmail.value = true
+    showPassword.value = false
+  }
+}
+
+const emitInputFn = () => {
+  if (emailValue.value === '') {
+    isErrorEmail.value = false
+    showPassword.value = false
+  }
+}
+</script>
 
 <template>
-  <div class="contanier">
+  <div
+    class="contanier"
+    flex items-center justify-center
+    h-100vh p-6
+  >
     <div class="login-card">
       <div class="column">
-        <h1>登录</h1>
-        <p>想来一局七圣召唤吗？请输入账号密码！</p>
-        <form>
-          <div class="form-item">
-            <input type="text" class="form-element" placeholder="用户名">
+        <h1 mb-5 text-4xl font-900>
+          登录
+        </h1>
+        <p>快登录账号，来一场七圣召唤吧！</p>
+        <p color-gray text-sm mt-1>
+          没有账号?输入邮箱将自动注册
+        </p>
+        <form mt-12>
+          <div>
+            <input
+              v-model="emailValue"
+              type="text"
+              input
+              placeholder="邮箱"
+              @change="emitChangeFn"
+              @input="emitInputFn"
+            >
           </div>
-          <div class="form-item">
-            <input type="password" class="form-element" placeholder="密码">
+          <div mt-4>
+            <input
+              v-model="password"
+              type="password"
+              input
+              placeholder="密码"
+            >
           </div>
-          <div class="form-checkbox-item">
-            <input id="rememberMe" type="checkbox" checked>
-            <label for="rememberMe">记住我</label>
-          </div>
-          <div class="flex">
-            <button type="button">
+          <div flex mt-20 justify-between items-center>
+            <button btn>
               登陆
             </button>
             <a href="#">忘记密码，点我重置！</a>
           </div>
-          <p style="margin-top: 3rem; margin-bottom: 1.5rem">
-            第三方账号登入
-          </p>
-          <div class="social-buttons">
-            <a href="#" class="wechat">
-              <i class="bi bi-wechat" />
-            </a>
-            <a href="#" class="twitter">
-              <i class="bi bi-twitter" />
-            </a>
-            <a href="#" class="github">
-              <i class="bi bi-github" />
-            </a>
-          </div>
         </form>
       </div>
-      <div class="column">
-        <h2>自然选择号欢迎您登舰！</h2>
-        <p>如果你没有账号，你想要现在注册一个吗？</p>
-        <a href="#">注册</a>
-      </div>
+      <div class="column" />
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 $theme-color: #88abaf;
-.contanier {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    padding: 1.5rem;
+.contanier:after {
+    content: "";
+    position: fixed;
+    inset: 0;
+    background-color: $theme-color;
+    width: 60%;
     height: 100vh;
-
-    &:after {
-        content: "";
-        position: fixed;
-        inset: 0;
-        background-color: $theme-color;
-        width: 60%;
-        height: 100vh;
-        clip-path: polygon(0 100%, 0 0, 100% 0, 70% 100%);
-        z-index: -1;
-    }
+    clip-path: polygon(0 100%, 0 0, 100% 0, 70% 100%);
+    z-index: -1;
 }
 
 .login-card {
     background-color: white;
     border: 1px solid #ddd;
     box-shadow: 0 10px 50px -30px black;
-    width: 1200px;
+    width: 70rem;
+    min-width: 370px;
     border-radius: 30px;
     overflow: hidden;
     display: grid;
-    grid-template-columns: auto auto;
-
-    a {
-        text-decoration: none;
-        color: $theme-color;
-    }
+    grid-template-columns: 1fr 1fr;
 
     .column {
         padding: 4rem;
 
         &:last-child {
-            background: url('./../../../public/logo/login_poster.png') center;
+            background: url('/logo/login_poster.png') center;
             background-size: cover;
             position: relative;
             display: flex;
@@ -108,123 +120,6 @@ $theme-color: #88abaf;
             * {
                 z-index: 1;
             }
-
-            a {
-                display: inline-block;
-                padding: 12px 30px;
-                font-size: 16px;
-                border: 2px solid $theme-color;
-                color: black;
-                border-radius: 50px;
-                cursor: pointer;
-                transition: all 0.3;
-                font-weight: 600;
-
-                &:hover {
-                    background-color: $theme-color;
-                    color: white;
-                }
-            }
-        }
-
-        &:last-child {
-            text-align: center;
-        }
-    }
-
-    h1 {
-        margin-bottom: 15px;
-    }
-
-    .form-element {
-        width: 100%;
-        border: none;
-        background-color: white;
-        padding: 20px 30px;
-        font-size: 18px;
-        border-radius: 50px;
-        transition: box-shadow 0.2s;
-
-        &:focus {
-            outline: none;
-            box-shadow: 0 0 0 2px $theme-color;
-        }
-    }
-
-    input[type=checkbox] {
-        accent-color: $theme-color;
-        width: 16px;
-        height: 16px;
-    }
-
-    form {
-        margin-top: 3rem;
-
-        &>* {
-            margin-top: 1rem;
-        }
-
-        button {
-            background-color: $theme-color;
-            color: white;
-            border: none;
-            padding: 20px 40px;
-            border-radius: 50px;
-            cursor: pointer;
-            font-weight: 600;
-            font-size: 18px;
-            transition: all 0.2s;
-
-            &:active {
-                scale: 0.95;
-                background-color: darken($theme-color, 10%);
-            }
-        }
-
-        .form-check-item {
-            display: flex;
-            align-items: center;
-            gap: 0.7rem;
-            margin-left: 5px;
-        }
-    }
-}
-
-.flex {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-.social-buttons {
-    display: flex;
-    gap: 1rem;
-
-    a {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 50px;
-        height: 50px;
-        border-radius: 50px;
-        font-size: 22px;
-        color: white;
-        background-color: green;
-
-        &.wechat {
-            background-color: #06ca5c;
-        }
-
-        &.twitter {
-            background-color: #55acee;
-        }
-
-        &.github {
-            background-color: #111;
-        }
-
-        &:hover {
-            scale: 0.95;
         }
     }
 }
@@ -238,7 +133,7 @@ $theme-color: #88abaf;
         display: block;
         width: 500px;
         text-align: center;
-        background: url('./../../../public/logo/login_poster.png') center;
+        background: url('/logo/login_poster.png') center;
         background-size: cover;
 
         .column {
@@ -246,15 +141,6 @@ $theme-color: #88abaf;
                 display: none;
             }
         }
-    }
-
-    .flex {
-        flex-direction: column;
-        gap: 1rem;
-    }
-
-    .social-buttons {
-        justify-content: center;
     }
 }
 </style>
